c64574371b4e577668e5a99b13177453543efe29
Erikkui <erik.kuitunen@outlook.com>
2024-10-04 01:01:23 +0300

    --

--- js/functionalities.js
+++ js/functionalities.js
@@ -1,23 +1,26 @@
-const createMap = async () => {
+async function createMap() {
     // Fetch map data
     const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
     const dataPromise = await fetch( url )
     const data = await dataPromise.json()
 
+    const [posData, negData] = await fetchMigrationData()
 
-    console.log(data)
+    console.log(posData)
 
-    initializeMap( data )
+    initializeMap( [data, posData, negData] )
 }
 
 function initializeMap(data) { 
+    const [mapData, posData, negData] = data
+    
     let map = L.map( "map" )
     let OpenStreetMap = L.tileLayer( 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
         minZoom: -3,      
         attribution: "© OpenStreetMap",                
     }).addTo( map )
 
-    let geoJSON = L.geoJSON( data, {
+    let geoJSON = L.geoJSON( mapData, {
         weight: 2,
         onEachFeature: municipalityTooltip,
     }).addTo( map )
@@ -36,10 +39,13 @@ async function fetchMigrationData() {
     const posURL = "https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f"
     const negURL = "https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e"
 
-    const positiveMigrationData = await fetch( posURL ).json()
-    const negativeMigrationData = await fetch( negURL ).json()
+    const positiveMigrationDataPromise = await fetch( posURL )
+    const positiveMigrationData = await positiveMigrationDataPromise.json()
 
-    return positiveMigrationData, negativeMigrationData
+    const negativeMigrationDataPromise = await fetch( negURL )
+    const negativeMigrationData = await negativeMigrationDataPromise.json()
+
+    return [ positiveMigrationData, negativeMigrationData ]
 }
 
 
