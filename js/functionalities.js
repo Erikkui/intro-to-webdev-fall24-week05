0558f958f6bfdaf8dfff7ce0c1673f8a70b15c64
Erikkui <erik.kuitunen@outlook.com>
2024-10-04 00:51:04 +0300

    --

--- js/functionalities.js
+++ js/functionalities.js
@@ -3,7 +3,10 @@ const createMap = async () => {
     const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
     const dataPromise = await fetch( url )
     const data = await dataPromise.json()
+
+
     console.log(data)
+
     initializeMap( data )
 }
 
@@ -24,8 +27,19 @@ function initializeMap(data) {
 
 const municipalityTooltip = (feature, layer) => {
     const muniName = feature.properties.name
-    // console.log(muniName)
+
+
     layer.bindTooltip( muniName )
+}
+
+async function fetchMigrationData() {
+    const posURL = "https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f"
+    const negURL = "https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e"
+
+    const positiveMigrationData = await fetch( posURL ).json()
+    const negativeMigrationData = await fetch( negURL ).json()
+
+    return positiveMigrationData, negativeMigrationData
 }
 
 
